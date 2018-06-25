ba43f113e83908abb44c85577533f0fbb2b4ab34
Monica Tamaru Jones <mntamaru@gmail.com>
2018-06-25 16:31:18 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
- 
-  get 'sessions/new'
 
-  root 'application#hello'
+  #root 'application#hello'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
