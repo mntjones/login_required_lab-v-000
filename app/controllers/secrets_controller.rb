40abb5269f47d41726da343f29699e0c00718f4a
Monica Tamaru Jones <mntamaru@gmail.com>
2018-06-25 16:15:00 +0000

    automatically backed up by learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,7 +1,7 @@
 class SecretsController < ApplicationController
   def show
     if !session[:name]
-      redirect_to '/'
+      redirect_to (controller: 'sessions', action: 'new')
     else
       :show
     end
